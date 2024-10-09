399e2e27bb192d8c55d9b0d4a60cd46205464b77
felix✌🏻 <162877035+schwienernitzel@users.noreply.github.com>
2024-10-09 14:09:52 +0200

    testing: Stub out Partei / Rolle, simplify Redner
    Signed-off-by: felix✌🏻 <162877035+schwienernitzel@users.noreply.github.com>

--- convert_test.py
+++ convert_test.py
@@ -18,36 +18,10 @@ def main(filename):
         if re.search('sitzung-datum', line):
             datum = re.sub('.*sitzung-datum="([^"]+)".*', r'\1', line)
 
-        # Rednername extrahieren
-        if re.search('<redner id=', line):
-            # Suche nach Vorname, Nachname und evtl. Titel
-            vorname = '-'
-            nachname = '-'
-            titel = ''
-
-            # Schleife durch die nächsten Zeilen, um Vorname, Nachname und Titel zu finden
-            j = i + 1
-            while j < len(content) and not re.search('</redner>', content[j]):
-                if re.search('<vorname>', content[j]):
-                    vorname = re.sub('.*<vorname>([^<]+)</vorname>.*', r'\1', content[j])
-                if re.search('<nachname>', content[j]):
-                    nachname = re.sub('.*<nachname>([^<]+)</nachname>.*', r'\1', content[j])
-                if re.search('<titel>', content[j]):
-                    titel = re.sub('.*<titel>([^<]+)</titel>.*', r'\1', content[j])
-                j += 1
-
-            # Rednername formatieren
-            if titel:
-                redner = f'{titel} {vorname} {nachname}'
-            else:
-                redner = f'{vorname} {nachname}'
-
-        # Partei/Fraktion extrahieren
-        if re.search('<fraktion>', line):
-            partei = re.sub('.*<fraktion>([^<]+)</fraktion>.*', r'\1', line)
-
-        if re.search('<rolle_lang>', line):
-            partei = re.sub('.*<rolle_lang>([^<]+)</rolle_lang>.*', r'\1', line)
+        # Redner. Format: </redner>Helmut Kleebank (SPD):</p>
+        if re.search('</redner>', line):
+            redner = re.sub('.*/redner>([^<]+).*', r'\1', line).strip()
+            redner = re.sub(':', '', redner).strip()
 
         # Rede-Absatz extrahieren
         if re.search('<p', line) and not re.search('<redner', line):
@@ -60,7 +34,7 @@ def main(filename):
         if re.search("rede id=", line):
             if rede_id != '-':
                 gesamte_rede = ' ## '.join(rede)
-                print_text += f'\n{rede_id}\t{datum}\t{redner}\t{partei}\t{gesamte_rede}'
+                print_text += f'\n{rede_id}\t{datum}\t{redner}\t{gesamte_rede}'
             rede_id = re.sub('.*rede id="([^"]+)".*', r'\1', line)
             rede = []
 
@@ -71,7 +45,7 @@ def main(filename):
     # Letzte Rede hinzufügen
     if rede_id != '-':
         gesamte_rede = ' ## '.join(rede)
-        print_text += f'\n{rede_id}\t{datum}\t{redner}\t{partei}\t{gesamte_rede}'
+        print_text += f'\n{rede_id}\t{datum}\t{redner}\t{gesamte_rede}'
 
     print(print_text)
 
